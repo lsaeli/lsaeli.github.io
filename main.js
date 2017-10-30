9607ec8178ffb987c9c09a0f29a5811f164e0458
Lily Saeli <lilysaeli@Lilys-MacBook-Pro.local>
2017-10-29 22:31:18 -0400

    fixed preload

---
+++ main.js
@@ -0,0 +1,21 @@
+function preloader() {
+	if (document.getElementById) {
+		document.getElementById("preload-01").style.background = "./images/opening.jpg";
+		// document.getElementById("preload-02").style.background = "url(http://domain.tld/image-02.png) no-repeat -9999px -9999px";
+		// document.getElementById("preload-03").style.background = "url(http://domain.tld/image-03.png) no-repeat -9999px -9999px";
+	}
+}
+function addLoadEvent(func) {
+	var oldonload = window.onload;
+	if (typeof window.onload != 'function') {
+		window.onload = func;
+	} else {
+		window.onload = function() {
+			if (oldonload) {
+				oldonload();
+			}
+			func();
+		}
+	}
+}
+addLoadEvent(preloader);
