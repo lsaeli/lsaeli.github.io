785c5b0b69e29d9ae6ccf7700041a11dfa1a44ad
Lily Saeli <lilysaeli@Lilys-MacBook-Pro.local>
2017-11-01 11:22:23 -0400

    added cat planet by popular request

--- warped-sun/nebula/cats.js
+++ warped-sun/nebula/cats.js
@@ -3,3 +3,6 @@ function click_handler2() { alert("click_handler2"); }
 document.getElementById('cats').addEventListener('click', function() {
     alert("In a distant universe, not far away, there's a place called the Milky Way, in which cats rule a planet called, 'Earth.'");
 }, false);
+document.getElementById('cats').addEventListener('click', function() {
+    alert("It's rumored that only Nebula Members can travel to Cat Planet.");
+}, false);
---
+++ warped-sun/nebula/p-info.js
@@ -0,0 +1,8 @@
+	function click_handler1() { alert("click_handler1"); }
+function click_handler2() { alert("click_handler2"); }
+document.getElementById('p-info').addEventListener('click', function() {
+    alert("Construction of the greatest planet ever began in AF 3942 (anno Feline).");
+}, false);
+document.getElementById('p-info').addEventListener('click', function() {
+    alert("It's made of yarn, blankets, slippers, giant boxes and catnip. This planet's construction was a generous contribution by the CLU (Cat Lovers Universal), where all speciesa and createures celebrate the joys of cats!");
+}, false);
